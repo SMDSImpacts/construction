15aed40bbbc7988ef9afbf7ae9c82a8a98f9af3e
SamChukwuemeka <engrsamchukwuemeka@gmail.com>
2023-10-23 02:15:18 +0100

    updated Next Config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       serverComponentsExternalPackages: ["mongoose"],
     },
     images: {
-      domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'],
+      domains: ['lh3.googleusercontent.com', 'res.cloudinary.com/smdsimages/image/upload/']
     },    
     webpack(config) {
       config.experiments = {
