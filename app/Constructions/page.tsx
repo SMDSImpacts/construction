b56e30ec7bc48af4110a759c41fc3beb560a8638
Sam Chukwuemeka <engrsamchukwuemeka@gmail.com>
2024-07-22 22:25:07 +0100

    Update page.tsx

--- app/Constructions/page.tsx
+++ app/Constructions/page.tsx
@@ -3,7 +3,7 @@ const Constructions = () => {
   return (
     <section>     
       <div className="flex content-center justify-between gap-4 mt-3 border-y-2 flex-1">
-        Construction
+        Constructions
       </div>
     </section>
   )
